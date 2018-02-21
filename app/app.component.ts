169c85c541f62bed0aafdf9ad97f3463ebdd3f63
danyel murray <danyel@danyels-MacBook-Pro.local>
2018-02-21 13:27:43 -0800

    finish angular2 assignment.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,34 +1,43 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
-  selector: 'app-root',
-  template: `
+	selector: 'app-root',
+	template: `
     <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-
-     <ul>
-       <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-     </ul>
-
-   </div>
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+      <hr>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+    	<new-task (newTaskSender)="addTask($event)"></new-task>
+    </div>
   `
 })
 
 export class AppComponent {
-  currentFocus: string = 'Angular Homework';
-  currentTime = new Date();
-  month: number = this.currentTime.getMonth() + 1;
-  day: number = this.currentTime.getDate();
-  year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
-  ];
-}
+	currentFocus: string = 'Angular Homework';
+	currentTime = new Date();
+	month: number = this.currentTime.getMonth() + 1;
+	day: number = this.currentTime.getDate();
+	year: number = this.currentTime.getFullYear();
+	selectedTask: Task = null;
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string) { }
+	masterTaskList: Task[] = [
+		// new Task('Finish weekend Angular homework for Epicodus course', 3),
+		// new Task('Begin brainstorming possible JavaScript group projects', 2),
+		// new Task('Add README file to last few Angular repos on GitHub', 2)
+	];
+
+	editTask(clickedTask) {
+		this.selectedTask = clickedTask;
+	};
+
+	finishedEditing() {
+		this.selectedTask = null;
+	}
+
+	addTask(newTaskFromChild: Task) {
+		this.masterTaskList.push(newTaskFromChild);
+	}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,25 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
+import { CompletenessPipe } from './completeness.pipe';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+	imports: [
+		BrowserModule,
+		FormsModule
+	], declarations: [
+		AppComponent,
+		TaskListComponent,
+		EditTaskComponent,
+		NewTaskComponent,
+		CompletenessPipe
+	], bootstrap: [
+		AppComponent
+	]
 })
 
 export class AppModule {
